51bcf53d2b3a7241c05c41ce66bcad4083affc2b
Ainhoa <113107823+AinhoaQM@users.noreply.github.com>
2023-11-06 19:17:34 +0000

    terminando el ejercico

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,59 +1,46 @@
 import React, { useState } from "react";
 
-
-//create your first component
 const Home = () => {
-	const [color, setColor] = useState("")
-	const randomLight = () => {
-		if(color === "red"){
-			setColor("yellow")
-		}
-		else if(color === "yellow"){
-			setColor("green")
-		}
-		else {
-			setColor("red")
-		}
+  const [color, setColor] = useState("");
 
-		/* switch (color) {
-		  case "red":
-			setColor("green");
-			break;
-		  case "yellow":
-			setColor("red");
-			break;
-		  case "green":
-			setColor("yellow");
-			break;
-		  default:
-			setColor("red");
-		} */
-	  };
-	  
-	  
+  const randomLight = () => {
+    if (color === "red") {
+      setColor("yellow");
+    } else if (color === "yellow") {
+      setColor("green");
+    } else if (color === "green") {
+      setColor("purple");
+    } else if (color === "purple") {
+      setColor("red");
+    } else {
+      setColor("red");
+    }
+  };
 
-	  return (
-		<div className="text-center">
-		  <div className="containerLight">
-			<div
-			  className={color === "red" ? "red light" : "red"}
-			  onClick={() => setColor("red")}
-			></div>
-			<div
-			  className={color === "yellow" ? "orange light" : "orange"}
-			  onClick={() => setColor("yellow")}
-			></div>
-			<div
-			  className={color === "green" ? "green light" : "green"}
-			  onClick={() => setColor("green")}
-			></div>
-		  </div>
-		  <div>
-			<button onClick={randomLight}>Cambiar luz</button>
-		  </div>
-		</div>
-	  );
-	  
+  const [purple, setPurple] = useState(false);
+
+  const togglePurpleLight = () => {
+    setPurple(!purple);
+  };
+
+  return (
+    <div className="container">
+		<div className="stick"></div>
+    	<div className={`container-fluid container-light ${ purple ? "with-purple" : "" }`} >
+    	<div className={color === "red" ? "red red-light" : "red"} onClick={() => setColor("red")}></div>
+        <div className={color === "yellow" ? "orange orange-light" : "orange"} onClick={() => setColor("yellow")}></div>
+        <div className={color === "green" ? "green green-light" : "green"} onClick={() => setColor("green")}></div>
+        {purple && (
+        	<div className={ color === "purple" ? "purple purple-light" : "purple" } onClick={() => setColor("purple")}></div>
+        )}
+    	</div>
+    	<div className="container col-6 button-container pt-5">
+			<button onClick={togglePurpleLight} className="btn btn-primary purple-button"> Luz morada </button>
+			<button onClick={togglePurpleLight} className="btn btn-primary remove-purple-button"> Quitar Luz Morada </button>
+			<button onClick={randomLight} className="btn btn-primary change-light-button"> Cambiar luz </button>
+    	</div>
+    </div>
+  );
 };
 
 export default Home;
